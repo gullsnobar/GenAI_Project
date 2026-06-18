53945bb604db5b7562a66223762b027e56dd9062
gullsnobar <abdulrehmanali7775@gmail.com>
2026-06-18 11:32:30 +0500

    login and signup pages are properly working

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,8 +1,12 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import Login from "./features/auth/pages/Login"
 import Register from "./features/auth/pages/Register"
 
 export const router = createBrowserRouter([
+     {
+        path: "/",
+        element: <Navigate to="/login" replace />
+     },
      {
         path: "/login",
         element: <Login/>
